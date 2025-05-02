2c703f0baec31c0fa4aad02396c75413f1937e3b
Tara Sankhalpara <tara.sankhalpara@charlottetilbury.com>
2025-05-02 17:21:24 +0100

    add in further customer details and styling

--- src/app/welcome/[id]/page.tsx
+++ src/app/welcome/[id]/page.tsx
@@ -6,13 +6,24 @@ export default function Welcome() {
   return (
      <div className={styles.page}>
         <main className={styles.main}>
+            <div className={styles.imgContainer}>
             <Image
                 src={Cat}
-                width={300}
-                height={200}
+                width={500}
+                height={400}
                 alt={"cute cat"}
+                className={styles.image}
             />
-            <h1>Your next delivery</h1>
+            </div>
+            <div className={styles.details}>
+            <h1>Your next delivery for Dorian and Ocie</h1>
+            <h2>{`Hey Kayleigh! In two days' time, we'll be charging you for your next order for Dorian and Ocie's fresh food.`}</h2>
+            <p>Total price: £134.00</p>
+            <div className={styles.actions}>
+            <a className={styles.button1}>See Details</a>
+            <a className={styles.button2}>Edit Delivery</a>
+            </div>
+            </div>
         </main>
    </div>
    );
