6977d019cb45d20e2c90db1337826ab416728efa
Tara Sankhalpara <tara.sankhalpara@charlottetilbury.com>
2025-05-02 16:57:43 +0100

    Add image and basic responsive layout

--- src/app/welcome/[id]/page.tsx
+++ src/app/welcome/[id]/page.tsx
@@ -1,6 +1,19 @@
+import Image from "next/image";
+import Cat from "./CuteCat.jpg";
+import styles from "./page.module.css";
 
 export default function Welcome() {
   return (
-    <h1>Your next delivery</h1>
+     <div className={styles.page}>
+        <main className={styles.main}>
+            <Image
+                src={Cat}
+                width={300}
+                height={200}
+                alt={"cute cat"}
+            />
+            <h1>Your next delivery</h1>
+        </main>
+   </div>
    );
 }
